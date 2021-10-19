2f953d91463207c5b837a48268c8c3080461e911
vgdaut <84093794+vgdaut@users.noreply.github.com>
2021-10-19 16:18:23 +0600

    Remove fsleep(unsigned long) definition, already defined in RHEL's kernel 4.18

--- main.h
+++ main.h
@@ -31,18 +31,18 @@
 #include <linux/etherdevice.h>
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0)
-/* see Documentation/timers/timers-howto.rst for the thresholds */
-static inline void fsleep(unsigned long usecs)
-{
-        if (usecs <= 10)
-                udelay(usecs);
-        else if (usecs <= 20000)
-                usleep_range(usecs, 2 * usecs);
-        else
-                msleep(DIV_ROUND_UP(usecs, 1000));
-}
-#endif
+// #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0)
+// /* see Documentation/timers/timers-howto.rst for the thresholds */
+// static inline void fsleep(unsigned long usecs)
+// {
+//         if (usecs <= 10)
+//                 udelay(usecs);
+//         else if (usecs <= 20000)
+//                 usleep_range(usecs, 2 * usecs);
+//         else
+//                 msleep(DIV_ROUND_UP(usecs, 1000));
+// }
+// #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 7, 0)
 #define NUM_NL80211_BANDS IEEE80211_NUM_BANDS
